24f2e3e354579d08394e6b6465317b8a0e42306c
Deng Li <ji3g4kami@gmail.com>
2017-03-19 11:55:00 +0800

    add pause

--- stopwatch.py
+++ stopwatch.py
@@ -2,17 +2,24 @@
 
 import time
 
-instructions = 'end'
+instructions = ' '
+time_passed = 0
 
-instructions = input('Enter "start" to start counting:')
-if instructions == 'start':
-	startTime = time.time()
 
-instructions = input('Enter "end" to stop counting:')
-if instructions == 'end':
-	endTime = time.time()
+while True:
+	instructions = input('Enter "start" to start counting:')
+	if instructions == 'start':
+		startTime = time.time()
 
-time_passed = round(endTime - startTime, 2)
+	instructions = input('Enter "pasue" to pause counting, "end" to stop counting:')
+	if instructions == 'pause':
+		pauseTime = time.time()
+		time_passed += round(pauseTime - startTime, 2)
+		continue
+	elif instructions == 'end':
+		endTime = time.time()
+		time_passed += round(endTime - startTime, 2)
+		break
 
 print(str(time_passed) + ' seconds has passed')
 
